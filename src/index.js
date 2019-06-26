d6a04f9ecef8aa64cbd175732016ff62cc7c15ab
jamesobrien86 <jamesobrien.web@gmail.com>
2019-06-26 10:57:02 +0300

    wip - get user location

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 
 
 const App = () => {
+
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err, "failed")
+    );
+
+
     return (
         <h1>Hello</h1>
     )
